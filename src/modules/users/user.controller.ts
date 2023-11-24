9150ad7518c7ac2cca19a5411ff983e9dbdcb8a8
Anita-Mahmud <anita.mahmud13@gmail.com>
2023-11-24 21:53:13 +0600

    deleted user

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,82 +1,100 @@
-
 import { Request, Response } from "express";
 import { UserService } from "./user.service";
 import userValidationSchema from "./user.validation";
 
 // create user controller
-const createUser = async (req:Request, res:Response)=>{
-    try{
-        const userData = req.body;
-        const {value} = userValidationSchema.validate(userData)
-        const result = await UserService.createUserInDB(value);
-        res.status(200).json({
-          success: true,
-          message: "User created successfully!",
-          data: result,
-        });
-      } catch (error) {
-        res.status(500).json({
-          success: false,
-          message: 'something went wrong',
-         
-        });
-      }
-    }
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const { value } = userValidationSchema.validate(userData);
+    const result = await UserService.createUserInDB(value);
+    res.status(200).json({
+      success: true,
+      message: "User created successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "something went wrong",
+    });
+  }
+};
 
 // retrieve users controller
-const getUsers = async (req:Request,res:Response)=>{
-  try{
+const getUsers = async (req: Request, res: Response) => {
+  try {
     const result = await UserService.getAllUsersFromDB();
     res.status(200).json({
       success: true,
       message: "Users fetched successfully!",
       data: result,
     });
-  }catch (error) {
+  } catch (error) {
     res.status(500).json({
       success: false,
-      message:  'something went wrong',
-      
+      message: "something went wrong",
     });
   }
-}
+};
 
 // retrieve single user controller
-const getSingleUser = async(req:Request,res:Response)=>{
-  try{
-    const {userId} = req.params;
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
     const result = await UserService.getSingleUserFromDB(userId);
     res.status(200).json({
       success: true,
       message: "User fetched successfully!",
       data: result,
-    })
-  }catch (error) {
+    });
+  } catch (error) {
     res.status(500).json({
       success: false,
-      message: error.message|| 'something went wrong',
+      message: error.message || "something went wrong",
     });
   }
-}
+};
 // update user controller
-const updateUser =async(req:Request,res:Response)=>{
-  try{
-    const {userId} =req.params;
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
     const userData = req.body;
-    const result = await UserService.updateUserInDB(userId,userData);
+    const result = await UserService.updateUserInDB(userId, userData);
     res.status(200).json({
       success: true,
       message: "User updated successfully!",
       data: result,
-      })
-      }catch (error) {
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "something went wrong",
+    });
+  }
+};
+// delete user controller
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const result = await UserService.deleteUserFromDB(userId);
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully!",
+      data: result,
+      });
+      } catch (error) {
         res.status(500).json({
           success: false,
-          message: error.message|| 'something went wrong',
+          message: error.message || "something went wrong",
           });
           }
-          }
+          };
 
-export const UserController ={
-    createUser,getUsers,getSingleUser,updateUser
-}
+export const UserController = {
+  createUser,
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser
+};
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -2,42 +2,52 @@ import { TUser } from "./user.interface";
 import { User } from "./user.model";
 
 // user create in db
-const createUserInDB = async(user: TUser)=>{
-    const userInDB = await User.create(user);
-    return userInDB;
-}
+const createUserInDB = async (user: TUser) => {
+  const userInDB = await User.create(user);
+  return userInDB;
+};
 
 // Retrieve a list of all users
-const getAllUsersFromDB = async()=>{
-    const users = await User.find().select('username fullName age email address');
-    return users;
-}
+const getAllUsersFromDB = async () => {
+  const users = await User.find().select("username fullName age email address");
+  return users;
+};
 
 //Retrieve a specific user by ID
-const getSingleUserFromDB = async(userId: number)=>{
-    if(await User.isUserExists(userId))
-    {
-        const user = User.findOne({userId}).select('-password -orders');
-        return user;
-    }
-    else{
-        throw new Error('User does not exists');
-    }
-}
-//Update a user by ID
-const updateUserInDB = async(userId: number, user: TUser)=>{
-    if(await User.isUserExists(userId))
-    {
-        const filter = { userId: userId };
-        const updatedUser = await User.findOneAndUpdate(filter, user, {new: true}).select('-password');
-            return updatedUser;
-            }
-            else{
-                throw new Error('User does not exists');
-                }
-}
-
+const getSingleUserFromDB = async (userId: number) => {
+  if (await User.isUserExists(userId)) {
+    const user = User.findOne({ userId }).select("-password -orders");
+    return user;
+  } else {
+    throw new Error("User does not exists");
+  }
+};
+//Update user information
+const updateUserInDB = async (userId: number, user: TUser) => {
+  if (await User.isUserExists(userId)) {
+    const filter = { userId: userId };
+    const updatedUser = await User.findOneAndUpdate(filter, user, {
+      new: true,
+    }).select("-password");
+    return updatedUser;
+  } else {
+    throw new Error("User does not exists");
+  }
+};
+//Delete user
+const deleteUserFromDB = async (userId: number) => {
+  if (await User.isUserExists(userId)) {
+    const user = await User.deleteOne({ userId });
+    return user;
+  } else {
+    throw new Error("User does not exists");
+  }
+};
 
 export const UserService = {
-    createUserInDB,getAllUsersFromDB,getSingleUserFromDB,updateUserInDB
-}
+  createUserInDB,
+  getAllUsersFromDB,
+  getSingleUserFromDB,
+  updateUserInDB,
+  deleteUserFromDB
+};
